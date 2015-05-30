e60355a559f169aed24c238a1e178557f04cdd6b
Sunil Pai <threepointone@gmail.com>
2015-05-30 21:01:55 +0530

    hot loading works

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-// import webpack from 'webpack';
-var path = require('path');
+import webpack from 'webpack';
+import path from 'path';
 
-module.exports = {
+let config = {
   devtool: 'source-map',
   target: 'web',
   entry: {
@@ -26,3 +26,26 @@ module.exports = {
   },
   plugins: []
 };
+
+if(process.env.HOT){
+  config = {
+    ...config,
+    devtool: 'eval',
+    entry: Object.keys(config.entry).reduce((o, key) => ({...o, [key]: [
+        'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server'
+      ].concat(config.entry[key])}), {}),
+    module: {...config.module,
+      loaders: [{
+        ...config.module.loaders[0],
+        loaders: ['react-hot'].concat(config.module.loaders[0].loaders)
+      }]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ].concat(config.plugins)
+  };
+}
+
+module.exports = config;
