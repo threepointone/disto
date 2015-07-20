0fda32d74ed3d5569e27020881919c88ab353fe5
Sunil Pai <threepointone@gmail.com>
2015-07-20 18:38:31 +0530

    prepare for middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { createRedux, bindActionCreators } from 'redux';
 import { Provider, Connector } from 'redux/react';
+
+import { createRedux, createDispatcher, composeStores, bindActionCreators } from 'redux';
+import thunkMiddleware from 'redux/lib/middleware/thunk';
 
 export class Flux extends React.Component{
   state = {
-    redux: createRedux(this.props.stores)
+    redux: this.props.redux || createRedux(createDispatcher(
+      composeStores(this.props.stores),
+      getState => [thunkMiddleware(getState)]
+    ))
   }
   render(){
     return <Provider redux={this.state.redux}>{
