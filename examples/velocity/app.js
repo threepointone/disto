39ea903d2fa12ad09d015c03c27473c5e4e214c3
Sunil Pai <threepointone@gmail.com>
2015-07-21 03:27:22 +0530

    .

--- examples/velocity/app.js
+++ examples/velocity/app.js
@@ -1,4 +1,4 @@
-import {Velocity} from './velocity'
+
 
 // a complete yakshave
 
@@ -7,12 +7,46 @@ import {Velocity} from './velocity'
 // react-springs smoothes out movements to give average velocity
 // all resulting in -
 
+import React from 'react';
+import {Flux} from '../../src';
+import {Spring} from 'react-motion';
+import debounce from 'debounce';
+
+const debounced = debounce(f => f(), 200),
+  move = evt => dispatch => dispatch({evt, type: 'event'}) && debounced(() => dispatch({type: 'settle'})),
+  tracker = (o = {x: 0, y: 0, velocity: 0, timeStamp: Date.now()}, {type, evt}) => {
+    if(type === 'event'){
+      let deltaX = o.x - evt.pageX,
+        deltaY = o.y - evt.pageY,
+        velocity = (evt.timeStamp === o.timeStamp) ? 0 :
+          Math.sqrt((deltaX * deltaX) + (deltaY * deltaY)) / (evt.timeStamp - o.timeStamp);
+
+      return { x: evt.pageX, y: evt.pageY, timeStamp: evt.timeStamp, velocity };
+    }
+    if(type === 'settle'){
+      return { ...o, velocity: 0};
+    }
+    return o;
+  };
+
+class Velocity{
+  render(){
+    return <Flux stores={{tracker}} actions={{move}}>{
+      ({tracker}, {move}) =>
+        <Spring endValue={{velocity: {val: tracker.velocity * 100} }}>{
+          ({velocity}) => this.props.children(velocity.val, move)
+        }</Spring>
+    }</Flux>;
+  }
+}
+
+
 export class App {
   render(){
     return <Velocity>{
-      (velocity, move) => <div style={{width:200, height:200}} onMouseMove={move}>
+      (velocity, move) => <div style={{width: 200, height: 200}} onMouseMove={move}>
         speed is {Math.round(velocity)}
       </div>
-    }</Velocity>
+    }</Velocity>;
   }
 }
